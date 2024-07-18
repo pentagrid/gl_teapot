0211034494060f94c3c90bcb62e602f4e70ddcc0
David Perek <lordperek@gmail.com>
2024-07-18 11:22:10 -0700

    initial utah teapot demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,90 @@
-#include <iostream>
+// This program is a flyby around the RGB color cube.  One intersting note
+// is that because the cube is a convex polyhedron and it is the only thing
+// in the scene, we can render it using backface culling only. i.e., there
+// is no need for a depth buffer.
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#ifdef __APPLE_CC__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+#include <cmath>
+
+// Display and Animation. To draw we just clear the window and draw the cube.
+// Because our main window is double buffered we have to swap the buffers to
+// make the drawing visible. Animation is achieved by successively moving our
+// camera and drawing. The function nextAnimationFrame() moves the camera to
+// the next point and draws. The way that we get animation in OpenGL is to
+// register nextFrame as the idle function; this is done in main().
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  GLfloat mat_diffuse[] = { 1.0, 0.0, 0.0, 1.0 }; 
+  glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
+  glutSolidTeapot(1.0);
+  glFlush();
+  glutSwapBuffers();
+}
+
+// We'll be flying around the cube by moving the camera along the orbit of the
+// curve u->(8*cos(u), 7*cos(u)-1, 4*cos(u/3)+2).  We keep the camera looking
+// at the center of the cube (0.5, 0.5, 0.5) and vary the up vector to achieve
+// a weird tumbling effect.
+void timer(int v) {
+  static GLfloat u = 0.0;
+  u += 0.01;
+  glLoadIdentity();
+  gluLookAt(8*cos(u), 7*cos(u)-1, 4*cos(u/3)+2, .5, .5, .5, cos(u), 1, 0);
+  glutPostRedisplay();
+  glutTimerFunc(1000/60.0, timer, v);
+}
+
+// When the window is reshaped we have to recompute the camera settings to
+// match the new window shape.  Set the viewport to (0,0)-(w,h).  Set the
+// camera to have a 60 degree vertical field of view, aspect ratio w/h, near
+// clipping plane distance 0.5 and far clipping plane distance 40.
+void reshape(int w, int h) {
+  glViewport(0, 0, w, h);
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  gluPerspective(60.0, GLfloat(w) / GLfloat(h), 0.5, 40.0);
+  glMatrixMode(GL_MODELVIEW);
+}
+
+// Function to set up lighting and material properties
+void initLighting() {
+    GLfloat light_ambient[] = { 0.2, 0.2, 0.2, 1.0 };  // Ambient light
+    GLfloat light_diffuse[] = { 0.8, 0.8, 0.8, 1.0 };  // Diffuse light
+    GLfloat light_specular[] = { 1.0, 1.0, 1.0, 1.0 }; // Specular light
+    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 }; // Light position
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glEnable(GL_LIGHTING);  // Enable lighting
+    glEnable(GL_LIGHT0);    // Enable light source 0
+    glEnable(GL_DEPTH_TEST); // Enable depth testing
+
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_shininess[] = { 50.0 };
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+}
+
+void init() {
+  glEnable(GL_DEPTH_TEST);
+  initLighting();
+}
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowSize(500, 500);
+  glutCreateWindow("Das Teapot");
+  glutReshapeFunc(reshape);
+  glutTimerFunc(100, timer, 0);
+  glutDisplayFunc(display);
+  init();
+  glutMainLoop();
 }
